c463112f170d90eb25a7f5253eed6ef273e69eae
Hatim Banswadawala <hatimhussainbans5253@gmail.com>
2025-07-19 06:03:03 -0700

    Walking Skeleton using React Vite

--- API/Program.cs
+++ API/Program.cs
@@ -6,15 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>( options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
 // Configure the HTTP request pipeline.
+app.UseCors(options =>
+{
+    options.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000");
+});
 
 app.MapControllers();
 
