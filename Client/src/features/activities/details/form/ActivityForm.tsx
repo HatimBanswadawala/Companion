9b77dc2e4bfe10f151bc8071baeb31ce8fa0d0cc
Hatim Banswadawala <hatimhussainbans5253@gmail.com>
2025-07-21 05:15:13 -0700

    CRUD using React Prop Drilling

---
+++ Client/src/features/activities/details/form/ActivityForm.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import type { FormEvent } from "react";
+
+type Props = {
+    activity?: Activity
+    closeForm: () => void
+    submitForm: (activity:Activity) => void
+}
+
+export default function ActivityForm({activity, closeForm, submitForm}:Props) {
+
+const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data: {[key:string]:FormDataEntryValue} = {};
+
+    const formdata = new FormData(event.currentTarget);
+
+    formdata.forEach((value,key)=>{
+        data[key]=value;
+    })
+
+    if (activity) data.id = activity.id;
+    submitForm(data as unknown as Activity);
+}
+  return (
+    <Paper sx={{borderRadius:3, padding:3}}>
+        <Typography variant='h5' gutterBottom color='primary'>
+            Create Activity
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
+            <TextField name="title" label='Title' defaultValue={activity?.title}/>
+            <TextField name="description" label='Description' multiline/>
+            <TextField name="category" label='Category'/>
+            <TextField name="date" label='Date' type="date" placeholder="dd-mm-yyyy" />
+            <TextField name="city" label='City'/>
+            <TextField name="venue" label='Venue'/>
+            <Box display='flex' justifyContent='end' gap={3}>
+                <Button color='inherit' onClick={closeForm}>Cancel</Button>
+                <Button type='submit' color='success' variant='contained'>Submit</Button>
+            </Box>
+        </Box>
+    </Paper>
+  )
+}
